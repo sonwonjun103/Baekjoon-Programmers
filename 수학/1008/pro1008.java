89f5e90ce6a649a858ee88d172c18bef5c9c369c
WON_JUN_SON <80496821+sonwonjun103@users.noreply.github.com>
2022-05-14 23:45:03 +0900

    Add files via upload

---
+++ 수학/1008/pro1008.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class pro1008 {
+    public static void main(String[] args){
+        double x;
+        double y;
+
+        Scanner sc=new Scanner(System.in);
+
+        x=sc.nextDouble();
+        y=sc.nextDouble();
+
+        System.out.println(x/y);
+    }
+
+}
