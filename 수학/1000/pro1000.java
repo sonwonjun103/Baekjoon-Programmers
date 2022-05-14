acfc0617f2983b38af0e5ec5e6b47c3330278f67
WON_JUN_SON <80496821+sonwonjun103@users.noreply.github.com>
2022-05-14 23:36:32 +0900

    Add files via upload

---
+++ 수학/1000/pro1000.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class pro1000 {
+    public static void main(String[] args){
+        int x;
+        int y;
+
+        Scanner sc=new Scanner(System.in);
+
+        x=sc.nextInt();
+        y=sc.nextInt();
+
+        System.out.println(x+y);
+    }
+
+}
